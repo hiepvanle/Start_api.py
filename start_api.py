44838f2481ee4db3fb75e5a9907b52440ac0f1e5
Lê Văn Hiệp <130354863+hiepvanle@users.noreply.github.com>
2023-04-13 08:44:21 +0700

    Update start_api.py

--- start_api.py
+++ start_api.py
@@ -3,44 +3,44 @@ from flask import Flask, jsonify, request, abort
 app = Flask(__name__)
 
 # Tạo dữ liệu ví dụ
-tracks = [
+MySong = [
     {'my_song_id': 1, 'my_name': 'Song 1'},
     {'my_song_id': 2, 'my_name': 'Song 2'}
 ]
 
 
-# Lấy danh sách tất cả các tracks
-@app.route('/tracks', methods=['GET'])
-def get_tracks():
-    return jsonify(tracks)
+# Lấy danh sách tất cả các MySong
+@app.route('/MySong', methods=['GET'])
+def get_MySong():
+    return jsonify(MySong)
 
 
 # Lấy một track theo ID
-@app.route('/tracks/<int:song_id>', methods=['GET'])
+@app.route('/MySong/<int:song_id>', methods=['GET'])
 def get_track(song_id):
-    track = [track for track in tracks if track['my_song_id'] == song_id]
+    track = [track for track in MySong if track['my_song_id'] == song_id]
     if len(track) == 0:
         abort(404)
     return jsonify(track[0])
 
 
 # Thêm một track mới
-@app.route('/tracks', methods=['POST'])
+@app.route('/MySong', methods=['POST'])
 def create_track():
     if not request.json or not 'my_name' in request.json:
         abort(400)
     track = {
-        'my_song_id': tracks[-1]['my_song_id'] + 1,
+        'my_song_id': MySong[-1]['my_song_id'] + 1,
         'my_name': request.json['my_name'],
     }
-    tracks.append(track)
+    MySong.append(track)
     return jsonify({'track': track}), 201
 
 
 # Cập nhật thông tin của một track theo ID
-@app.route('/tracks/<int:track_id>', methods=['PUT'])
+@app.route('/MySong/<int:track_id>', methods=['PUT'])
 def update_track(song_id):
-    track = [track for track in tracks if track['my_song_id'] == song_id]
+    track = [track for track in MySong if track['my_song_id'] == song_id]
     if len(track) == 0:
         abort(404)
     if not request.json:
@@ -50,12 +50,12 @@ def update_track(song_id):
 
 
 # Xóa một track theo ID
-@app.route('/tracks/<int:song_id>', methods=['DELETE'])
+@app.route('/MySong/<int:song_id>', methods=['DELETE'])
 def delete_track(song_id):
-    track = [track for track in tracks if track['my_song_id'] == song_id]
+    track = [track for track in MySong if track['my_song_id'] == song_id]
     if len(track) == 0:
         abort(404)
-    tracks.remove(track[0])
+    MySong.remove(track[0])
     return jsonify({'result': True})
 
 
