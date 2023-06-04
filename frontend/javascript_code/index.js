acf8362a740da306f1806f72781ebfa0a5620ae5
NemBadBoizzz <namlien39@gmail.com>
2023-06-05 03:57:35 +0700

    update front end

--- frontend/javascript_code/index.js
+++ frontend/javascript_code/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+const sidebarTabs = $$('.js__sidebar-tabs')
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
-
-
+sidebarTabs.forEach((tab, index) => {
+    tab.onclick = function(){
+      $('.js__sidebar-tabs.sidebar__item--active').classList.remove('sidebar__item--active');
+                tab.classList.add('sidebar__item--active');
+                containerPanes[0].style.display = "none";
+                containerPanes[1].style.display = "none";
+                containerPanes[2].style.display = "none";
+                containerPanes[index].style.display = "block";
+    }
+  
+  })
