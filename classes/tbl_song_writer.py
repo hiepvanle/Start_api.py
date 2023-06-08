f93f8436434b77c41b16d741261e061bfe69498a
VY <vyluongtts123@gmail.com>
2023-06-08 15:08:08 +0700

    update

--- classes/tbl_song_writer.py
+++ classes/tbl_song_writer.py
@@ -9,43 +9,83 @@ class Song_writer(Resource):
         self.connections = kwargs['connections']
 
     def get(self):
-        if 'song_writer_id' != "":
-            cur = self.connections.cursor()
-            drive = []
-            sql = "SELECT * FROM tbl_song_writer"
-            cur.execute(sql)
-            result = cur.fetchall()
-            for i in result:
-                data = {
-                    'song_writer_id': i[0],
-                    'writer_date': i[1],
-                    'write_id': i[3],
-                }
-                drive.append(data)
-            return jsonify(result)
+        if request.query_string is not None or request.query_string != "":
+            with self.connections.cursor() as cursor:
+                # get all
+                if request.args['swid'] == "*":
+                    drive = []
+                    sql = "SELECT * FROM `tbl_song_writer`"
+                    cursor.execute(sql)
+                    result = cursor.fetchall()
+                    for i in result:
+                        data = {
+                            'song_writer_id': i[0],
+                            'write_date': i[1].strftime('%Y-%m-%d %H:%M:%S'),  # Convert datetime to string
+                            'writer_id': i[2],
+                            # 'hometown': i[3],
+                            # 'date_of_birth': i[4].strftime('%Y-%m-%d %H:%M:%S'),  # Convert datetime to string
+                        }
+                        drive.append(data)
+                    return drive, 200
+                # get by id
+                else:
+                    sql = "SELECT * FROM `tbl_song_writer` WHERE `song_writer_id`=%s"
+                    cursor.execute(sql, (request.args['swid'],))
+                    result = cursor.fetchone()
+                    if result is not None:
+                        data = {
+                            'song_writer_id': result[0],
+                            'write_date': result[1].strftime('%Y-%m-%d %H:%M:%S'),  # Convert datetime to string
+                            'writer_id': result[2],
+                            # 'singer_id': result[0],
+                            # 'singer_name': result[1],
+                            # 'singer_description': result[2],
+                            # 'hometown': result[3],
+                            # 'date_of_birth': result[4].strftime('%Y-%m-%d %H:%M:%S'),  # Convert datetime to string
+                        }
+                        return data, 200
+                    else:
+                        return {"status": "not found"}, 404
         else:
-            cur = self.connections.cursor()
-            sql = "SELECT * FROM tbl_song_writer WHERE song_writer_id=%s"
-            drive = []
-            cur.execute(sql, ('song_writer_id',))
-            result = cur.fetchone()
-            for i in result:
-                data = {
-                    'song_writer_id': i[0],
-                    'writer_date': i[1],
-                    'write_id': i[3],
-                }
-                drive.append(data)
-            return jsonify(result)
+            return {"status": "error"}, 400
+    # def get(self):
+    #     if 'song_writer_id' != "":
+    #         cur = self.connections.cursor()
+    #         drive = []
+    #         sql = "SELECT * FROM tbl_song_writer"
+    #         cur.execute(sql)
+    #         result = cur.fetchall()
+    #         for i in result:
+    #             data = {
+    #                 'song_writer_id': i[0],
+    #                 'writer_date': i[1],
+    #                 'write_id': i[3],
+    #             }
+    #             drive.append(data)
+    #         return jsonify(result)
+    #     else:
+    #         cur = self.connections.cursor()
+    #         sql = "SELECT * FROM tbl_song_writer WHERE song_writer_id=%s"
+    #         drive = []
+    #         cur.execute(sql, ('song_writer_id',))
+    #         result = cur.fetchone()
+    #         for i in result:
+    #             data = {
+    #                 'song_writer_id': i[0],
+    #                 'writer_date': i[1],
+    #                 'write_id': i[3],
+    #             }
+    #             drive.append(data)
+    #         return jsonify(result)
 
     def post(self):
         if request.is_json:
             # convert to json
             data = request.get_json(force=True)["data"]
             with self.connections.cursor() as cursor:
-                sql_insert = "INSERT INTO tbl_song_writer (song_writer_id, writer_date, write_id) " \
+                sql_insert = "INSERT INTO tbl_song_writer (song_writer_id, write_date, writer_id) " \
                            "VALUES ('{}', '{}','{}');"
-                sql_post = sql_insert.format(data['song_writer_id'], data['writer_date'], data['write_id'])
+                sql_post = sql_insert.format(data['song_writer_id'], data['write_date'], data['writer_id'])
                 print(sql_post)
                 cursor.execute(sql_post)
                 self.connections.commit()
@@ -56,7 +96,7 @@ class Song_writer(Resource):
     def delete(self):
         if request.is_json:
             # convert to json
-            data = request.get_json(force=True)
+            data = request.get_json(force=True)["data"]
             song_writer_id = data['song_writer_id']
             with self.connections.cursor() as cursor:
                 sql_delete = "DELETE FROM tbl_song_writer WHERE song_writer_id=%s"
