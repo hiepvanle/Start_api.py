314455c24be5566b38448119b4e744992f36445b
NemBadBoizzz <namlien39@gmail.com>
2023-06-09 06:06:16 +0700

    update

--- frontend/javascript_code/album.js
+++ frontend/javascript_code/album.js
@@ -1,6 +1,12 @@
-async function logJSONData() {
-    const response = await fetch("http://127.0.0.1:5000/album?aid=*");
-    const jsonData = await response.json();
-    console.log(jsonData);
-  }
 
+getDatafromAlbumAPI();
+
+async function getDatafromAlbumAPI() {
+    try {
+        const responseAPI = await fetch('http://127.0.0.1:5000/album?aid=*');
+        const data = await responseAPI.json();
+        console.log('data :>>', data);
+    } catch (error) {
+        console.error('An error occurred while fetching data from the album API: ', error);
+    }
+}
