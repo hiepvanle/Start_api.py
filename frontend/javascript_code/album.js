3a9c2f2352441ddfbd702d3888a91c7e47b322a7
NemBadBoizzz <namlien39@gmail.com>
2023-06-09 08:36:11 +0700

    update

--- frontend/javascript_code/album.js
+++ frontend/javascript_code/album.js
@@ -1,7 +1,7 @@
 getDataFromAlbumAPI();
 
 async function getDataFromAlbumAPI() {
-    const responseAPI = await fetch('http://127.0.0.1:5000/album?aid=*' {mode: 'no-cors'});
+    const responseAPI = await fetch('http://127.0.0.1:5000/album?aid=*');
     const data = await responseAPI.json();
     console.log(':>>>> data', data);
 }
