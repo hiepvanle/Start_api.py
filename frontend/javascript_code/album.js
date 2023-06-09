4f893160600e3cc1ce787e49dd7a70f48def7645
NemBadBoizzz <namlien39@gmail.com>
2023-06-10 00:40:29 +0700

    update javascript code

--- frontend/javascript_code/album.js
+++ frontend/javascript_code/album.js
@@ -1,7 +1,11 @@
-getDataFromAlbumAPI();
-
-async function getDataFromAlbumAPI() {
-    const responseAPI = await fetch('http://127.0.0.1:5000/album?aid=*');
-    const data = await responseAPI.json();
-    console.log(':>>>> data', data);
+searchAlbum();
+async function searchAlbum() {
+    const results = await fetch(`http://127.0.0.1:5000/album?aid=*`, {mode: 'no-cors'});
+    const data = await results.json();
+    const album = data.albums.items[0];
+    return {
+    title: album.name,
+    artist: album.artists[0].name,
+    coverArtUrl: album.images[0].url,
+};
 }
